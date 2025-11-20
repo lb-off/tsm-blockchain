d42e6676c23a8c3a1dcf6631ca5151e38fc92860
Louis Bertucci <louisbertucci@hotmail.fr>
2025-11-20 23:06:01 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Dauphine-Blockchain class
+
+This repo contains smart contracts for the blockchain economics class at Paris-Dauphine University.
+
+It contains the Solidity code of 2 tokens (the DauCoin and the PhineCoin), as well as a simplified version of an AMM similar to UniSwap v2 with a Constant Product pricing function.
+
+In the AMM repository, there is also a file `LToken.sol` which is used by the AMM at deploy time to create a liquidity token that can be used by Liquidity Providers to redeem their liquidity.
+
+The repo contains a file `addresses.txt` with the addresses of deployed contracts on both the Goerli and Sepolia testnets.
+
+For questions, please contact : louis.bertucci@institutlouisbachelier.org
---
+++ Tokens/PhineCoin.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+//import the default ERC20 implementation by openzeppelin
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/*
+The contract extends the ERC20 contract to be mintable by anyone but up to a certain limit
+defined by the contract owner.
+*/
+contract PhineCoin is ERC20 {
+    /*
+    This section defines the internal variable of the smart contract. Recall that because
+    the smart contract is ERC20, it also inherits all the variables (and functions) of ERC20.sol.
+    */
+    mapping(address => uint256) private minted;
+    uint256 public total_minted;
+    uint256 public max_mint;
+    uint256 public max_mint_per_user;
+
+    constructor(uint256 _max_mint, uint256 _max_mint_per_user) ERC20("Phine_coin", "PHC") {
+        /*stores some parameters. 
+        max_mint is the max total amount to be minted.
+        max_mint_per_user si the total amount a user can mint.
+        */
+        max_mint = _max_mint;
+        max_mint_per_user = _max_mint_per_user;
+    }
+
+    function mint(uint256 amount) external {        
+        //define 1 variables to be used in this function
+        address account = msg.sender;
+        // make a number of checks to make sure, we do not over-mint.
+        require(total_minted < max_mint, "Total supply has already been minted");
+        require(total_minted + amount <= max_mint, "Minting this much would exceed the max supply");
+        require(minted[account] < max_mint_per_user, "User has already minted max");
+        require(minted[account] + amount <= max_mint_per_user, "User cannot mint this much");
+        //actually mint only if all checks pass. The _mint function is defined in ERC20.
+        _mint(account, amount);
+        //stored the value minted by this user and the total value minted.
+        minted[account] += amount;
+        total_minted += amount;
+    }
+}
